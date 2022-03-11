b2561e4ccbd72c69431302cd0664dd58d32da21d
Bruno Filho <oliveira.brunofilho@gmail.com>
2022-03-10 23:40:26 -0300

    feito a página para renderização do post

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
+          <ActiveLink activeClassName={styles.active} href='/'>
+          <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href='/posts'>
           <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
